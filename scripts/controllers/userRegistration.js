40e2f565cdf2a3d9c1e7bc2a17dde7b7feff2eee
radsconnects527 <radhika.u.nag@gmail.com>
2016-05-25 22:56:05 -0400

    user registration events

--- scripts/controllers/userRegistration.js
+++ scripts/controllers/userRegistration.js
@@ -1,6 +1,22 @@
 angular.module('freebirdApp')
-  .controller('UserRegistrationCtrl', ['$scope', '$location', '$http', '$rootScope', function ($scope, $location, $http, $rootScope) {
-	  
-	  
-	  
-  }]);
+    .controller('UserRegistrationCtrl', ['$scope', '$location', '$http', '$rootScope', function ($scope, $location, $http, $rootScope) {
+
+            $scope.user = {};
+        
+            $scope.createUser = function () {
+                var data =  JSON.stringify($scope.user);
+          /*      $http.post("/event/createUser/", data).success(function (data, status) {
+                    console.log("user created successfully");
+                })*/
+                console.log(data);
+                
+
+            }
+            
+            $scope.validatePassword = function (){
+                
+                console.log("fgh");
+                
+            }
+
+            }]);
